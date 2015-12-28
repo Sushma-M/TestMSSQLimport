393e1e64c9f07f9e720a49ca35f5fcb4b0c61bdf
sushma maddirala <sushma.maddirala+30@wavemaker.com>
2015-12-28 06:05:17 +0000

    New files added - [VemployeeService.java,VjobCandidateEmploymentController.java,EmployeeDepartmentHistoryController.java,VemployeeDepartmentController.java,VemployeeDepartmentHistoryDao.java,AdventureWorks2014.spring.xml,Shift.java,TableNameController.java,googleapis_apiTarget.json,en.json,prefab-icon.png,VjobCandidateId.java,AdventureWorks2014_API.json,Vemployee.java,app.variables.json,VemployeeDepartmentId.java,EmployeePayHistoryServiceImpl.java,config.json,EmployeePayHistoryController.java,googleapis_API.json,VemployeeServiceImpl.java,TableName.java,JobCandidate.java,AdventureWorks2014_published_dataModel.json,VjobCandidateEducationService.java,EmployeePayHistoryDao.java,project-rest-service.xml,ShiftDao.java,EmployeeService.java,JobCandidateController.java,googlemaps-prefab-services.xml,EmployeeDepartmentHistory.java,googlemaps.jar,Employee.java,EmployeePayHistoryService.java,VjobCandidateEducationId.java,favicon.ico,googleapis_connection_settings.json,DepartmentServiceImpl.java,app.css,EmployeeDao.java,VjobCandidateEducationController.java,EmployeeDepartmentHistoryId.java,VjobCandidateEducationDao.java,AdventureWorks2014QueryExecutorServiceImpl.java,EmployeeDepartmentHistoryService.java,VemployeeDepartmentServiceImpl.java,VemployeeDepartmentDao.java,Main.js,types.js,Department.java,VjobCandidateServiceImpl.java,VemployeeDepartmentHistoryController.java,page.min.html,intercept-urls.json,package-info.java,prefab-dispatcher-servlet.xml,VjobCandidateEducation.java,DepartmentController.java,Main.html,Table13.java,DepartmentService.java,app.js,JobCandidateServiceImpl.java,AdventureWorks2014_query.json,EmployeeDepartmentHistoryDao.java,VemployeeDepartmentService.java,Table13ServiceImpl.java,Table13Controller.java,VjobCandidateEmployment.java,Main.css,VjobCandidateDao.java,ShiftService.java,JobCandidateDao.java,AdventureWorks2014-procedures.mappings.json,VemployeeController.java,EmployeeDepartmentHistoryServiceImpl.java,AdventureWorks2014QueryExecutorService.java,VjobCandidateEmploymentServiceImpl.java,EmployeePayHistoryId.java,.wmproject.properties,servicedef.xml,VjobCandidateEmploymentId.java,AdventureWorks2014_vk.hbm.xml,Table13Dao.java,AdventureWorks2014_procedure.json,VemployeeId.java,JobCandidateService.java,VjobCandidateEmploymentService.java,VjobCandidateEducationServiceImpl.java,db-connection-settings.json,VemployeeDepartmentHistoryServiceImpl.java,TableNameServiceImpl.java,AdventureWorks2014ProcedureExecutorService.java,VemployeeDepartmentHistory.java,EmployeeController.java,DepartmentDao.java,ProcedureExecutionController.java,googleapis.spring.xml,VemployeeDepartmentHistoryId.java,index.html,AdventureWorks2014ProcedureExecutorServiceImpl.java,Table13Service.java,googleapis_apiTarget.json,angular-google-maps.min.js,AdventureWorks2014-queries.hbm.xml,prefabPropertiesMetadata.json,AdventureWorks2014.properties,VjobCandidateService.java,TableNameService.java,EmployeePayHistory.java,QueryExecutionController.java,ShiftServiceImpl.java,VjobCandidateController.java,VjobCandidate.java,ShiftController.java,VemployeeDepartment.java,VemployeeDepartmentHistoryService.java,VemployeeDao.java,EmployeeServiceImpl.java,Main.variables.json,VjobCandidateEmploymentDao.java,securityService.spring.xml,servicedef.xml,prefabProperties.properties,TableNameDao.java] Files modified - [Main.variables.json,Main.html,production.properties,Main.js,project-services.xml,page.min.html,wmProperties.js,app.variables.json,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

---
+++ services/AdventureWorks2014/src/com/testmssqlimport/adventureworks2014/service/AdventureWorks2014ProcedureExecutorService.java
@@ -0,0 +1,23 @@
+/*\/**
+ * Copyright (c) 2015-2016 wavemaker.com All Rights Reserved.
+ * This software is the confidential and proprietary information of wavemaker-com * You shall not disclose such Confidential Information and shall use it only in accordance
+ * with the terms of the source code license agreement you entered into with wavemaker.com *\/*/
+
+
+package com.testmssqlimport.adventureworks2014.service;
+
+import java.util.Map;
+import java.util.List;
+
+
+import com.wavemaker.runtime.data.model.CustomProcedure;
+import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
+
+public interface AdventureWorks2014ProcedureExecutorService {
+
+	
+	List<Object> executeWMCustomProcedure(CustomProcedure procedure) ;
+
+
+}
+
